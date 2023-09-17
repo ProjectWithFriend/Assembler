ead7d22d7710e00b0f1b5a81637cc2a89e1fd42c
AuJung <90750969+AuJUngZ@users.noreply.github.com>
2023-09-17 12:46:17 +0700

    Aujungz/tokenizer (#1)
    * update tokenizer
    
    * update tokenizer

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,29 @@
-import Tokenizer.Tokenizer;
-import Tokenizer.IterateTokenizer;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import Tokenizer.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
-        tokenizer test = new IterateTokenizer("test");
-        System.out.println(test.comsume);
+//        read input.txt file
+        File file = new File("input.txt");
+        StringBuilder sb = new StringBuilder();
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                sb.append(line);
+                sb.append("\n");
+            }
+            scanner.close();
+            String src = sb.toString();
+//            System.out.println(src);
+            Tokenizer tokenizer = new IterateTokenizer(src);
+            while(tokenizer.hasNext()) {
+                System.out.println(tokenizer.consume());
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
