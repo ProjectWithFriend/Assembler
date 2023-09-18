b571a963c830d616f688319b482cf2679d37af2c
AuJung <90750969+AuJUngZ@users.noreply.github.com>
2023-09-18 20:12:49 +0700

    remake tokenizer (#4)

--- src/Tokenizer/IterateTokenizer.java
+++ src/Tokenizer/IterateTokenizer.java
@@ -1,18 +1,41 @@
 package Tokenizer;
 
+
+import java.util.ArrayList;
+
 public class IterateTokenizer implements Tokenizer {
+    private enum instruction {
+        add, nand, lw, sw, beq, jalr, halt, noop
+    };
+
     private final String src;
     private String next;
     private String prev;
     private int pos;
-
-
     private boolean isFirst = true;
+    private final ArrayList<ArrayList<String>> mappingInstruction;
+    private boolean isFirstLine = true;
+    private int filledField = 0;
+    private int line = 0;
+
+    private boolean findEnum(String s) {
+        for (instruction i : instruction.values()) {
+            if (i.name().equals(s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<ArrayList<String>> getMappingInstruction() {
+        return mappingInstruction;
+    }
 
 
     public IterateTokenizer(String src) {
         this.src = src;
         pos = 0;
+        mappingInstruction = new ArrayList<>();
         computeNext();
     }
 
@@ -62,60 +85,126 @@ public class IterateTokenizer implements Tokenizer {
         }
     }
 
-//    @Override
-//    public int getLine() {
-//        return line;
-//    }
-
     private void processSingleLineComment() {
         while (pos < src.length() && src.charAt(pos) != '\n') {
             pos++;
         }
+        //next line
+        pos++;
+        filledField = 0;
+        line++;
+        isFirst = true;
+        mappingInstruction.add(new ArrayList<>());
     }
 
 
     private void processWhiteSpace() {
-        while (pos < src.length() && Character.isWhitespace(src.charAt(pos))) {
+        while (pos < src.length() && Character.isWhitespace(src.charAt(pos)) && src.charAt(pos) != '\n') {
             pos++;
         }
     }
 
-
+    private void processSkipSpaceAndComment(){
+        if (src.charAt(pos) == '\n') { // No comment
+            pos = pos < src.length()+1 ? pos + 1 : pos;
+            line++;
+            filledField = 0;
+            isFirst = true;
+            mappingInstruction.add(new ArrayList<>());
+        } else { // comment
+            processSingleLineComment();
+        }
+    }
 
     private void computeNext() {
         if (src == null) return;
         StringBuilder sb = new StringBuilder();
 
-        //ignore whitespace
+        //if first line create arraylist just for first line
+        if (isFirstLine) {
+            mappingInstruction.add(new ArrayList<>());
+            isFirstLine = false;
+        }
 
+        //ignore whitespace
         processWhiteSpace();
 
-
+        //if end of file
         if (pos == src.length()) {
             prev = next;
             next = null;
             return;
         }
 
-        char c = src.charAt(pos);
-        if (c == '#') { //comment
-            processSingleLineComment();
-            isFirst = true;
-            pos++;
+        if (filledField != 3) { // It's not comment part
+            while (pos < src.length() && !Character.isWhitespace(src.charAt(pos))) {
+                sb.append(src.charAt(pos));
+                pos++;
+            }
+//            .fill command
+            if (sb.toString().equals(".fill")) {
+                mappingInstruction.get(line).add(sb.toString());
+                filledField = 2;
+            }else if(prev != null && prev.equals(".fill")){
+                mappingInstruction.get(line).add(sb.toString());
+                filledField = 3;
+            }else{
+
+                if (!findEnum(sb.toString()) && isFirst) { // It's label
+                    if (pos < src.length() && Character.isDigit(src.charAt(pos))) {
+                        throw new TokenizerException.BadCharacter(src.charAt(pos));
+                    } else { // It's label
+                        mappingInstruction.get(line).add(sb.toString());
+                        isFirst = false;
+                    }
+                } else if (findEnum(sb.toString())) { // It's instruction
+                    if (sb.toString().equals("noop") || sb.toString().equals("halt")) {
+                        mappingInstruction.get(line).add(sb.toString());
+                        processWhiteSpace();
+                        processSkipSpaceAndComment();
+                    }else{
+                        mappingInstruction.get(line).add(sb.toString());
+                        isFirst = false;
+                    }
+                } else { // It's field
+                    mappingInstruction.get(line).add(sb.toString());
+                    filledField++;
+                    isFirst = false;
+                }
+            }
+        } else { //
             processWhiteSpace();
+            processSkipSpaceAndComment();
         }
 
-        if (isFirst && pos < src.length() && Character.isDigit(src.charAt(pos))) {
-            throw new TokenizerException.BadCharacter(src.charAt(pos));
-        }
-
-        while (pos < src.length() && !Character.isWhitespace(src.charAt(pos))) {
-            sb.append(src.charAt(pos));
-            pos++;
-        }
 
         prev = next;
-        next = sb.isEmpty()? null : sb.toString();
-        isFirst = pos < src.length() && src.charAt(pos) == '\n';
+        next = sb.toString();
+
+
+//        //ignore whitespace
+//        processWhiteSpace();
+//        if (pos == src.length()) {
+//            prev = next;
+//            next = null;
+//            return;
+//        }
+//        char c = src.charAt(pos);
+//        if (c == '#') { //comment
+//            processSingleLineComment();
+//            isFirst = true;
+//            pos++;
+//            processWhiteSpace();
+//        }
+//        if (isFirst && pos < src.length() && Character.isDigit(src.charAt(pos))) {
+//            throw new TokenizerException.BadCharacter(src.charAt(pos));
+//        }
+//        while (pos < src.length() && !Character.isWhitespace(src.charAt(pos))) {
+//            sb.append(src.charAt(pos));
+//            pos++;
+//        }
+//        prev = next;
+//        next = sb.toString();
+//        isFirst = pos < src.length() && src.charAt(pos) == '\n';
     }
 }
