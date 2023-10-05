947fba1b5e19be31d2c68d81465f64da853c9b11
AuJung <nuttapong6280@gmail.com>
2023-10-05 18:46:51 +0700

    add comment of Tokenizer.

--- src/Tokenizer/IterateTokenizer.java
+++ src/Tokenizer/IterateTokenizer.java
@@ -8,16 +8,22 @@ public class IterateTokenizer implements Tokenizer {
         add, nand, lw, sw, beq, jalr, halt, noop
     }
 
-    private final String src;
-    private String next;
-    private String prev;
-    private int pos;
-    private boolean isFirst = true;
-    private final ArrayList<ArrayList<String>> mappingInstruction;
-    private boolean isFirstLine = true;
-    private int filledField = 0;
+    private final String src; // source code to tokenize
+    private String next; // the next token (char)
+    private String prev; // the previous token (char)
+    private int pos; // current position in the source
+    private boolean isFirst = true; // used to check if it's first of new line
+    private final ArrayList<ArrayList<String>> mappingInstruction; // mapping instruction
+    private boolean isFirstLine = true; //used to check if it's first line of source code
+    private int filledField = 0; //used to track how many field is filled
     private int line = 0;
 
+    /**
+     * Check if given string is in enum
+     *
+     * @param s given string
+     * @return true if given string is in enum
+     */
     private boolean findEnum(String s) {
         for (instruction i : instruction.values()) {
             if (i.name().equals(s)) {
@@ -27,16 +33,24 @@ public class IterateTokenizer implements Tokenizer {
         return false;
     }
 
+    /**
+     * @return mapping instruction
+     */
     public ArrayList<ArrayList<String>> getMappingInstruction() {
         return mappingInstruction;
     }
 
 
+    /**
+     * Constructor
+     *
+     * @param src source code to tokenize
+     */
     public IterateTokenizer(String src) {
         this.src = src;
         pos = 0;
-        mappingInstruction = new ArrayList<>();
-        computeNext();
+        mappingInstruction = new ArrayList<>(); // initialize mapping instruction
+        computeNext(); // start computation of next token
     }
 
     @Override
@@ -71,6 +85,12 @@ public class IterateTokenizer implements Tokenizer {
         }
     }
 
+    /**
+     * Consume the next token and check if it is the same as the given string
+     *
+     * @param s given string
+     * @return true if it is the same as the given string
+     */
     @Override
     public boolean consume(String s) {
         if (!hasNext()) {
@@ -85,6 +105,9 @@ public class IterateTokenizer implements Tokenizer {
         }
     }
 
+    /**
+     * Skip the comment
+     */
     private void processSingleLineComment() {
         while (pos < src.length() && src.charAt(pos) != '\n') {
             pos++;
@@ -98,29 +121,38 @@ public class IterateTokenizer implements Tokenizer {
     }
 
 
+    /**
+     * Skip the whitespace
+     */
     private void processWhiteSpace() {
         while (pos < src.length() && Character.isWhitespace(src.charAt(pos)) && src.charAt(pos) != '\n') {
             pos++;
         }
     }
 
-    private void processSkipSpaceAndComment(){
-        if (src.charAt(pos) == '\n') { // No comment
-            pos = pos < src.length()+1 ? pos + 1 : pos;
+    /**
+     * Skip the whitespace and comment
+     */
+    private void processSkipSpaceAndComment() {
+        if (src.charAt(pos) == '\n') { // if there is no comment prepare for next line
+            pos = pos < src.length() + 1 ? pos + 1 : pos;
             line++;
             filledField = 0;
             isFirst = true;
             mappingInstruction.add(new ArrayList<>());
-        } else { // comment
+        } else { // if there is comment skip it
             processSingleLineComment();
         }
     }
 
+    /**
+     * Main method to compute next token
+     */
     private void computeNext() {
         if (src == null) return;
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(); // use string builder to build string because it's easier to append or insert, and it's mutable and faster than string
 
-        //if first line create arraylist just for first line
+        //if first line of source code, create arraylist just for first line
         if (isFirstLine) {
             mappingInstruction.add(new ArrayList<>());
             isFirstLine = false;
@@ -137,37 +169,35 @@ public class IterateTokenizer implements Tokenizer {
         }
 
         if (filledField != 3) { // It's not comment part
-            while (pos < src.length() && !Character.isWhitespace(src.charAt(pos))) {
+            while (pos < src.length() && !Character.isWhitespace(src.charAt(pos))) { // append char to construct string of that word
                 sb.append(src.charAt(pos));
                 pos++;
             }
 //            .fill command
-            if (sb.toString().equals(".fill")) {
+            if (sb.toString().equals(".fill")) { // if it's .fill command
+                mappingInstruction.get(line).add(sb.toString()); // add .fill to mapping instruction
+                filledField = 2; //update filled field to 2 because .fill need only one field after it
+            } else if (prev != null && prev.equals(".fill")) { // if it's field after .fill
                 mappingInstruction.get(line).add(sb.toString());
-                filledField = 2;
-            }else if(prev != null && prev.equals(".fill")){
-                mappingInstruction.get(line).add(sb.toString());
-                filledField = 3;
-            }else{
-
+                filledField = 3; //update for tell that is end of field
+            } else {
                 if (!findEnum(sb.toString()) && isFirst) { // It's label
-                    if (pos < src.length() && Character.isDigit(src.charAt(pos))) {
+                    if (pos < src.length() && Character.isDigit(src.charAt(pos))) { // if label start with number
                         throw new TokenizerException.BadCharacter(src.charAt(pos));
                     } else { // It's label
                         mappingInstruction.get(line).add(sb.toString());
                         isFirst = false;
                     }
                 } else if (findEnum(sb.toString())) { // It's instruction
-                    if (sb.toString().equals("noop") || sb.toString().equals("halt")) {
+                    if (sb.toString().equals("noop") || sb.toString().equals("halt")) { // if it's noop or halt
                         mappingInstruction.get(line).add(sb.toString());
                         processWhiteSpace();
                         processSkipSpaceAndComment();
-                    }else if(sb.toString().equals("jalr")){
+                    } else if (sb.toString().equals("jalr")) { // if it's jalr
                         mappingInstruction.get(line).add(sb.toString());
                         isFirst = false;
                         filledField = 1;
-                    }
-                    else{
+                    } else { // if it's other instruction, it needs 3 field type R,I
                         mappingInstruction.get(line).add(sb.toString());
                         isFirst = false;
                     }
@@ -177,7 +207,7 @@ public class IterateTokenizer implements Tokenizer {
                     isFirst = false;
                 }
             }
-        } else { //
+        } else { // process comment of .fill
             processWhiteSpace();
             processSkipSpaceAndComment();
         }
